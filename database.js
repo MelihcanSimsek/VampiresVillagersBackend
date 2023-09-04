fd0d65ca0c66254b1baba27b3b22d31de1d37197
Melihcan Şimşek <melihsimsek02@gmail.com>
2023-09-04 16:31:43 +0300

    Vote added

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
-const {Client} = require('pg');
+const { Client } = require('pg');
 
 const client = new Client({
-    host:"localhost",
-    user:"postgres",
-    port:5432,
-    password:"password",
-    database:"vampirevillager"
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "password",
+  database: "vampirevillager"
 })
 
 client.connect();
@@ -13,123 +13,278 @@ client.connect();
 
 //PLAYER TABLE
 async function AddPlayer(id, name, gameId) {
-    try {
-    
-  
-      const insertQuery = 'INSERT INTO "Player" (Id, Name, GameId) VALUES ($1, $2, $3)';
-      const values = [id, name, gameId];
-  
-      await client.query(insertQuery, values);
-  
-      console.log('Oyuncu eklendi');
-    } catch (error) {
-      console.error('Hata:', error); 
-    } 
+  try {
+
+
+    const insertQuery = 'INSERT INTO "Player" (Id, Name, GameId) VALUES ($1, $2, $3)';
+    const values = [id, name, gameId];
+
+    await client.query(insertQuery, values);
+
+    console.log('Oyuncu eklendi');
+  } catch (error) {
+    console.error('Hata:', error);
+  }
+}
+
+async function DeletePlayer(id) {
+  try {
+
+    const deleteQuery = 'DELETE FROM "Player" WHERE Id=$1';
+    const values = [id];
+
+    await client.query(deleteQuery, values);
+
+    console.log('Oyuncu silindi');
+  } catch (error) {
+    console.error('Hata:', error);
+  }
+}
+
+async function GetPlayersbyGameId(gameId) {
+  try {
+
+
+    const selectQuery = 'SELECT Id,Name from "Player" WHERE GameId=$1';
+    const values = [gameId];
+
+    const result = await client.query(selectQuery, values);
+    return result.rows;
+
+  } catch (error) {
+    console.error('Hata:', error);
+  }
+}
+
+async function GetPlayerBySocketId(socketId) {
+  try {
+    const selectQuery = 'SELECT Id,Name,GameId from "Player" WHERE Id=$1';
+    const values = [socketId];
+
+    const result = await client.query(selectQuery, values);
+    return result;
+
+  } catch (error) {
+    console.error('Hata:', error);
+  }
+}
+
+//Chat Table
+async function AddChat(name, gameId, message) {
+
+  try {
+    const currentTime = new Date();
+
+    const hour = currentTime.getHours();
+    const minute = currentTime.getMinutes();
+    const second = currentTime.getSeconds();
+
+    const formattedTime = `${hour}:${minute}:${second}`;
+
+    const insertQuery = 'INSERT INTO "Chat" (Id,Name,GameId,Message,CreationDate ) VALUES (DEFAULT, $1,$2, $3,$4)';
+    const values = [name, gameId, message, formattedTime];
+    await client.query(insertQuery, values);
+  } catch (error) {
+    console.error('Hata:', error);
+  }
+}
+
+async function DeleteChat(gameId) {
+  try {
+    const deleteQuery = 'DELETE FROM "Chat" WHERE GameId=$1';
+    const values = [gameId];
+    await client.query(deleteQuery, values);
+  } catch (error) {
+    console.error('Hata:', error);
   }
 
-  async function DeletePlayer(id)
-  {
-    try {
-    
-        const deleteQuery = 'DELETE FROM "Player" WHERE Id=$1';
-        const values = [id];
-    
-        await client.query(deleteQuery, values);
-    
-        console.log('Oyuncu silindi');
-      } catch (error) {
-        console.error('Hata:', error);
-      }
+}
+
+async function GetChatMessagebyGameId(gameId) {
+  try {
+    const selectQuery = 'SELECT Name,Message FROM "Chat" WHERE GameId = $1';
+    const values = [gameId];
+    const result = await client.query(selectQuery, values);
+    return result;
+  } catch (error) {
+    console.error('Hata:', error);
   }
+}
 
-  async function GetPlayersbyGameId(gameId)
-  {
-    try {
-    
-    
-        const selectQuery = 'SELECT Id,Name from "Player" WHERE GameId=$1';
-        const values = [gameId];
-    
-        const result = await client.query(selectQuery, values);
-        return result.rows;
-        
-      } catch (error) {
-        console.error('Hata:', error);
-      } 
+//Game Table
+
+async function AddGame(gameId, gameSettings) {
+  try {
+    const insertQuery = 'INSERT INTO "Game" (Id,DayTime,NightTime,VampireNumber,PriestNumber,Day ) VALUES ($1,$2,$3,$4,$5,1)';
+    const values = [gameId, gameSettings.dayTime, gameSettings.nightTime, gameSettings.vampireNumber, gameSettings.priestNumber];
+    await client.query(insertQuery, values);
+
+  } catch (error) {
+    console.error('Hata:', error);
   }
+}
 
-  async function GetPlayerBySocketId(socketId)
-  {
-    try{
-      const selectQuery = 'SELECT Id,Name,GameId from "Player" WHERE Id=$1';
-      const values = [socketId];
-     
-      const result = await client.query(selectQuery, values);
-      return result;
 
-    }catch(error)
-    {
-      console.error('Hata:', error);
-    }
+async function GetGame(gameId) {
+  try {
+    const selectQuery = 'SELECT * FROM "Game" WHERE Id = $1';
+    const values = [gameId];
+    const result = await client.query(selectQuery, values);
+    return result;
+
+  } catch (error) {
+    console.error('Hata:', error)
   }
+}
 
-  //Chat Table
-  async function AddChat(name,gameId,message)
-  {
-    
-    try{
-      const currentTime = new Date();
+async function DeleteGame(gameId) {
+  try {
+    const deleteQuery = 'DELETE FROM "Game" WHERE Id=$1';
+    const values = [gameId];
+    await client.query(deleteQuery, values);
+  } catch (error) {
+    console.error('Hata:', error);
+  }
+}
+//ROLE Table Villager role 1, Vampire role 2, Priest role 3,
+async function AddRole(gameId,playerListWithRole)
+{
+  try {
+     await  playerListWithRole.forEach(player => {
+      const insertQuery = 'INSERT INTO "GameState" (Id,Name,GameId,Role,Live) VALUES ($1,$2,$3,$4,$5)';
+      const values = [player.id, player.name, gameId, player.role,true];
+      client.query(insertQuery, values);
+    });
+   
+  } catch (error) {
+    console.error('Hata:', error);
+  }
+}
+
+
+
+async function GetPlayersRoleByGameId(gameId)
+{
+  try {
+    const selectQuery = 'SELECT id,name,role,live FROM "GameState" WHERE GameId = $1';
+    const values = [gameId];
+    const result = await client.query(selectQuery, values);
+    return result;
+  } catch (error) {
+    console.error('Hata:', error)
+  }
+}
+
+async function GetPlayerRoleBySocketId(id)
+{
+  try {
+    const selectQuery = 'SELECT id,name,role,live FROM "GameState" WHERE Id = $1';
+    const values = [id];
+    const result = await client.query(selectQuery, values);
+    return result;
+  } catch (error) {
+    console.error('Hata:', error)
+  }
+}
+
+async function DeleteRole(gameId)
+{
+  try {
+    const deleteQuery = 'DELETE FROM "GameState" WHERE GameId=$1';
+    const values = [gameId];
+    await client.query(deleteQuery, values);
+
+  } catch (error) {
+    console.error('Hata:', error);
+  }
+}
+
+//VOTE Table
+// playerId:string;
+//     targetId:string;
+//     day:number;
+//     dayType:Boolean;
+//     gameId:string;
+async function AddVote(vote)
+{
+  try {
       
-      const hour = currentTime.getHours();
-      const minute = currentTime.getMinutes();
-      const second = currentTime.getSeconds();
+   const insertQuery = 'INSERT INTO "Vote" (Id,PlayerId,TargetId,Day,DayType,GameId) VALUES (DEFAULT,$1,$2,$3,$4,$5)';
+   const values = [vote.playerId, vote.targetId, vote.day, vote.dayType,vote.gameId];
+   await client.query(insertQuery, values);
+  
+ } catch (error) {
+   console.error('Hata:', error);
+ }
+}
 
-      const formattedTime = `${hour}:${minute}:${second}`;
+async function DeleteVote(gameId)
+{
+  try {
+    const deleteQuery = 'DELETE FROM "Vote" WHERE GameId=$1';
+    const values = [gameId];
+    await client.query(deleteQuery, values);
 
-      const insertQuery = 'INSERT INTO "Chat" (Id,Name,GameId,Message,CreationDate ) VALUES (DEFAULT, $1,$2, $3,$4)';
-      const values = [name, gameId, message,formattedTime];
-      await client.query(insertQuery, values);
-    }catch(error)
-    {
-      console.error('Hata:', error);
-    }
+  } catch (error) {
+    console.error('Hata:', error);
   }
+}
 
-  async function DeleteChat(gameId)
-  {
-    try{
-      const deleteQuery = 'DELETE FROM "Chat" WHERE GameId=$1';
-      const values = [gameId];
-      await client.query(deleteQuery, values);
-    }catch(error)
-    {
-      console.error('Hata:', error);
-    }
-
+async function GetAllVote(vote)
+{
+  try {
+    const selectQuery = 'SELECT playerid,targetid FROM "Vote" WHERE GameId = $1 AND Day=$2 AND DayType=$3';
+    const values = [vote.gameId,vote.day,vote.dayType];
+    const result = await client.query(selectQuery, values);
+    return result;
+  } catch (error) {
+    console.error('Hata:', error)
   }
+}
 
-  async function GetChatMessagebyGameId(gameId)
-  {
-    try{
-     const selectQuery = 'SELECT Name,Message FROM "Chat" WHERE GameId = $1';
-     const values = [gameId];
-     const result =  await client.query(selectQuery, values);
-     return result;
-    }catch(error)
-    {
-      console.error('Hata:', error);
-    }
+async function GetVote(vote)
+{
+  try {
+    const selectQuery = 'SELECT * FROM "Vote" WHERE GameId = $1 AND Day=$2 AND DayType=$3 AND PlayerId=$4';
+    const values = [vote.gameId,vote.day,vote.dayType,vote.playerId];
+    const result = await client.query(selectQuery, values);
+    return result;
+  } catch (error) {
+    console.error('Hata:', error)
   }
+}
+
+async function UpdateVote(vote)
+{
+  try {
+    const updateQuery = 'UPDATE  "Vote" SET TargetId=$1 WHERE GameId = $2 AND Day=$3 AND DayType=$4 AND PlayerId=$5'
+    const values = [vote.targetId,vote.gameId,vote.day,vote.dayType,vote.playerId];
+    await client.query(updateQuery, values);
+   
+  } catch (error) {
+    console.error('Hata:', error);
+  }
+}
 
 
-
-
-  module.exports = {
-    AddPlayer:AddPlayer,
-    DeletePlayer:DeletePlayer,
-    GetPlayersbyGameId:GetPlayersbyGameId,
-    GetPlayerBySocketId:GetPlayerBySocketId,
-    AddChat:AddChat,
-    DeleteChat:DeleteChat,
-    GetChatMessagebyGameId:GetChatMessagebyGameId
-  };
+module.exports = {
+  AddPlayer: AddPlayer,
+  DeletePlayer: DeletePlayer,
+  GetPlayersbyGameId: GetPlayersbyGameId,
+  GetPlayerBySocketId: GetPlayerBySocketId,
+  AddChat: AddChat,
+  DeleteChat: DeleteChat,
+  GetChatMessagebyGameId: GetChatMessagebyGameId,
+  DeleteGame: DeleteGame,
+  AddGame: AddGame,
+  GetGame: GetGame,
+  AddRole:AddRole,
+  DeleteRole:DeleteRole,
+  GetPlayerRoleBySocketId:GetPlayerRoleBySocketId,
+  GetPlayersRoleByGameId:GetPlayersRoleByGameId,
+  AddVote:AddVote,
+  DeleteVote:DeleteVote,
+  GetAllVote:GetAllVote,
+  UpdateVote:UpdateVote,
+  GetVote:GetVote
+};
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const  game  = require("./game");
+const game = require("./game");
 const db = require("./database.js");
 const app = require("express");
 const { AssignRole } = require("./game");
 const PORT = 3000;
 const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer,{
-    cors:true,
-    origin:["*"]
+const io = require("socket.io")(httpServer, {
+  cors: true,
+  origin: ["*"]
 });
 
 const usersData = {};
@@ -14,145 +14,169 @@ const chatMessage = [];
 
 
 
-  function getPlayerListByGameId(gameId) {
-    const playerList = [];
-    
-    for (const key in usersData) {
-      if (usersData.hasOwnProperty(key) && usersData[key].gameId === gameId) {
-        playerList.push({userName:usersData[key].nickname,id:key});
-      }
+function getPlayerListByGameId(gameId) {
+  const playerList = [];
+
+  for (const key in usersData) {
+    if (usersData.hasOwnProperty(key) && usersData[key].gameId === gameId) {
+      playerList.push({ userName: usersData[key].nickname, id: key });
     }
-    
-    return playerList;
   }
 
-
-
-  function getChatMessageByGameId(gameId) {
-    const messages = [];
-    
-    for (const message of chatMessage) {
-      if (message.gameId === gameId) {
-        messages.push({ userName: message.nickname, message: message.message });
-      }
-    }
-    
-    return messages;
+  return playerList;
 }
 
-io.on("connection",(socket)=>{
-    
-    
-    socket.on('disconnect', () => {
-        
-           db.GetPlayerBySocketId(socket.id).then(player=>{
-            db.DeletePlayer(socket.id).then(()=>{
-              db.GetPlayersbyGameId(player.rows[0].gameid).then(players=>{
 
-                
-                socket.broadcast.emit("leaveGame",{
-                  userName:player.rows[0].name,
-                  gameId:player.rows[0].gameid,
-                  playerList:players
-             })
-              })
 
-            })
-        });
-        
-        console.log("disconnected");
-    }  
-      );
+function getChatMessageByGameId(gameId) {
+  const messages = [];
 
-    socket.on("joinGame",data=>{
-        db.AddPlayer(socket.id,data.nickname,data.gameId).then(()=>{
-        db.GetPlayersbyGameId(data.gameId).then(players=>{
-          db.GetChatMessagebyGameId(data.gameId).then(messages =>{
-            console.log("index");
-            console.log(messages.rows);
-            socket.broadcast.emit("joinGame",{
-              userName:data.nickname+"",
-              gameId:data.gameId,
-              playerList:players
-          });
-  
-          socket.emit('joinGame', {
-              userName:data.nickname+"",
-              gameId:data.gameId,
-              playerList:players
-          });
-  
-          socket.emit('chat', {
-            gameId:data.gameId,
-            messages:messages.rows
-        });
-        io.to(socket.id).emit('id',socket.id);
+  for (const message of chatMessage) {
+    if (message.gameId === gameId) {
+      messages.push({ userName: message.nickname, message: message.message });
+    }
+  }
+
+  return messages;
+}
+
+io.on("connection", (socket) => {
+
+
+  socket.on('disconnect', () => {
+
+    db.GetPlayerBySocketId(socket.id).then(player => {
+      db.DeletePlayer(socket.id).then(() => {
+        db.GetPlayersbyGameId(player.rows[0].gameid).then(players => {
+
+
+          socket.broadcast.emit("leaveGame", {
+            userName: player.rows[0].name,
+            gameId: player.rows[0].gameid,
+            playerList: players
           })
         })
-        })
+
+      })
     });
 
-    socket.on("chat",data=>{
-      db.GetPlayerBySocketId(socket.id).then(player=>{
-        console.log(socket.id)
-        console.log(player.rows[0].name+" "+player.rows[0].gameid,+" "+data.message);
-        
-        db.AddChat(player.rows[0].name,player.rows[0].gameid,data.message).then(()=>{
-          
-          db.GetChatMessagebyGameId(player.rows[0].gameid).then(messages=>{
-            console.log(messages.rows);
-            
-            socket.broadcast.emit("chat",{
-                gameId:player.rows[0].gameid,
-                messages:messages.rows
+    console.log("disconnected");
+  }
+  );
+
+  socket.on("joinGame", data => {
+    db.AddPlayer(socket.id, data.nickname, data.gameId).then(() => {
+      db.GetPlayersbyGameId(data.gameId).then(players => {
+        db.GetChatMessagebyGameId(data.gameId).then(messages => {
+          console.log("index");
+          console.log(messages.rows);
+          socket.broadcast.emit("joinGame", {
+            userName: data.nickname + "",
+            gameId: data.gameId,
+            playerList: players
+          });
+
+          socket.emit('joinGame', {
+            userName: data.nickname + "",
+            gameId: data.gameId,
+            playerList: players
+          });
+
+          socket.emit('chat', {
+            gameId: data.gameId,
+            messages: messages.rows
+          });
+          io.to(socket.id).emit('id', socket.id);
+        })
+      })
+    })
+  });
+
+  socket.on("chat", data => {
+    db.GetPlayerBySocketId(socket.id).then(player => {
+      console.log(socket.id)
+      console.log(player.rows[0].name + " " + player.rows[0].gameid, +" " + data.message);
+
+      db.AddChat(player.rows[0].name, player.rows[0].gameid, data.message).then(() => {
+
+        db.GetChatMessagebyGameId(player.rows[0].gameid).then(messages => {
+          console.log(messages.rows);
+
+          socket.broadcast.emit("chat", {
+            gameId: player.rows[0].gameid,
+            messages: messages.rows
+          });
+
+          socket.emit('chat', {
+            gameId: player.rows[0].gameid,
+            messages: messages.rows
+          });
+
+        }, error => {
+          console.log(error);
+        })
+
+
+      })
+    })
+
+
+
+  })
+
+  socket.on("startGame", data => {
+    db.GetPlayersbyGameId(data.gameId).then(players => {
+      db.AddGame(data.gameId,data.gameSettings).then(()=>{
+        db.AddRole(data.gameId,game.AssignRole(players, data.gameSettings)).then(()=>{
+          db.GetPlayersRoleByGameId(data.gameId).then(playerListWithRole=>{
+            console.log(playerListWithRole.rows);
+            playerListWithRole.rows.forEach(element => {
+              io.to(element.id).emit('role', element.role);
             });
-           
-            socket.emit('chat', {
-                gameId:player.rows[0].gameid,
-                messages:messages.rows
-            });
-  
-          },error=>{
-            console.log(error);
+            socket.broadcast.emit("startGame", {
+              gameId: data.gameId,
+              playerList: playerListWithRole.rows,
+              gameSettings:data.gameSettings,
+              day:1
+            })
+            socket.emit("startGame", {
+              gameId: data.gameId,
+              playerList: playerListWithRole.rows,
+              gameSettings:data.gameSettings,
+              day:1
+            })
           })
-
-
         })
       })
+      })
+    })
+
+    socket.on("vote",data=>{
+      db.GetVote(data).then(voteResponse=>{
+        if(voteResponse.rows.length <= 0 )
+        {
+          db.AddVote(data).then(()=>{
+
+
+          })
+        }
+        else
+        {
+          db.UpdateVote(data).then(()=>{
+
+          })
+        }
+      })
+
       
-    
-
     })
 
-    socket.on("startGame",data=>{
-      db.GetPlayersbyGameId(data.gameId).then(players=>{
-
-        const playerListWithRole = game.AssignRole(players,data.gameSettings);
-        console.log(playerListWithRole);
-        playerListWithRole.forEach(element => {
-          io.to(element.id).emit('role',element.role);
-        });
-        
-  
-        socket.broadcast.emit("startGame",{
-          gameId:data.gameId,
-          playerList:playerListWithRole
-        })
-  
-        socket.emit("startGame",{
-          gameId:data.gameId,
-          playerList:playerListWithRole
-        })
-
-      })
-    
-    })
-})
+  })
 
 
 
 
 
-httpServer.listen(PORT,()=>{
-    console.log("Server started !!!");
+httpServer.listen(PORT, () => {
+  console.log("Server started !!!");
 })
